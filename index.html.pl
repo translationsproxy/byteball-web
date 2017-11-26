d12172904479bf4e9d4b6075328279e513625e84
translationsproxy <to.ny991@gmail.com>
2017-11-26 21:32:41 +0300

    New translations index.html (Polish)

--- index.html.pl
+++ index.html.pl
@@ -157,7 +157,7 @@
 		<div id="why" class="container">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2">
-					<h2 class="text-center">Dlaczego używać Byteball?</h2>
+					<h2 class="text-center">Dlaczego warto używać Byteball?</h2>
 				</div>
 			</div>
 		</div>
@@ -175,7 +175,7 @@
 								<p class="lead text-left">Byteball pozwala robić coś, czego nie potrafią tradycyjne waluty: <a href="https://medium.com/byteball/making-p2p-great-again-fe9e20546a4a"
 								 target="_blank">warunkowe płatności.</a></p>
 
-								<p>You set a condition when the payee receives the money. If the condition is not met, you get your money back.</p>
+								<p>Ustanawiasz warunek, kiedy odbiorca otrzyma pieniądze. Jeśli warunek nie zostanie spełniony, otrzymujesz zwrot pieniędzy.</p>
 								<div class="space"></div>
 							</div>
 							<div class="col-md-6">
@@ -199,12 +199,11 @@
 						 <div class="space"></div> 
 						<div class="row">
 							<div class="col-md-4 col-md-push-6">
-							<h2 class="title text-left">P2P insurance</h2>
-							<p class="lead text-left">Hedge against negative events.</p>
-							<p>Buy insurance from peers to get paid in case a negative event occurs. Sell insurance for profit.</p>
-							<p>Insurance is just <a href="https://medium.com/byteball/making-p2p-great-again-episode-iv-p2p-insurance-cbbd1e59d527"
-								 target="_blank">a simple smart contract</a> that can be unlocked by the insured &mdash; if the event in question
-								did occur, or by the insurer &mdash; otherwise.</p>
+							<h2 class="title text-left">Ubezpieczenia P2P</h2>
+							<p class="lead text-left">Zabezpieczenie przed negatywnymi zdarzeniami.</p>
+							<p>Kup ubezpieczenie od innych użytkowników, aby otrzymać płatność na wypadek negatywnych zdarzeń. Albo sprzedawaj ubezpieczenia dla zysku.</p>
+							<p>Ubezpieczenie to po prostu <a href="https://medium.com/byteball/making-p2p-great-again-episode-iv-p2p-insurance-cbbd1e59d527"
+								 target="_blank">inteligentny kontrakt</a>, który może zostać odblokowany przez ubezpieczonego &mdash; gdy wystąpi dane zdarzenie, albo przez ubezpieczyciela &mdash; jeśli dane zdarzenie nie wystąpi.</p>
 							<p>To insure against flight delay, find your counterpart in <a href="https://byteball.slack.com/messages/C54CDJATB/"
 								 target="_blank">#p2p_insurance</a> channel on our <a href="http://slack.byteball.org" target="_blank" title="Slack">Slack</a>,
 								create a contract, and if your flight arrives late, <a href="byteball:AuP4ngdv0S/rok+IaW1q2D6ye72eXLl3h+CqXNXzkBXn@byteball.org/bb#0000">chat with flight delays oracle</a>								to have it post the data about the delay, then unlock the contract.</p>
