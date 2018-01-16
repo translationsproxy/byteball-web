83a5aa810998c3b8141add8fa342797a5db87845
translationsproxy <to.ny991@gmail.com>
2018-01-17 00:02:43 +0300

    New translations index.html (Czech)

--- index.html.cs
+++ index.html.cs
@@ -141,7 +141,7 @@
 					<p class="lead what-is">When a contract is created on Byteball, it just works exactly as agreed upon.  Trust in the counterparty and reliance on the slow and expensive conventional legal system are not required any more as the contracts are enforced by the immutable rules of the decentralized platform.  See below the types of contracts available today.</p-->
 					
 					<p class="lead what-is">Byteball umožňuje důvěřovat tam, kde doposud důvěra nemohla existovat.</p>
-					<p class="lead what-is">Když je smlouva vytvořena v rámci platformy Byteball, je možné věřit, že vše proběhne přesně podle dohody.  Proč? Because it is validated by multiple nodes on the decentralized network, which all follow the same immutable rules.  The counterparty, even if it is a total stranger, has to behave honestly because only the rules have authority.  Such a contract is called a smart contract.  See below for examples of working smart contracts available today.</p>
+					<p class="lead what-is">Když je smlouva vytvořena v rámci platformy Byteball, je možné věřit, že vše proběhne přesně podle dohody.  Proč? Protože je v decentralizované síti potvrzena mnoha uzly, které mají všechny stejná neměnná pravidla.  Protistrana, i kdyby to byl úplný cizinec, se musí chovat čestně, protože pouze pravidla mají pravomoc.  Taková smlouva je nazývána smart kontrakt.  Níže naleznete příklady fungující smart kontraktů, které jsou již dostupné.</p>
 					
 					
 				</div>
@@ -157,7 +157,7 @@
 		<div id="why" class="container">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2">
-					<h2 class="text-center">Why use Byteball?</h2>
+					<h2 class="text-center">Proč využívat Byteball?</h2>
 				</div>
 			</div>
 		</div>
@@ -171,11 +171,11 @@
 						 <div class="space"></div> 
 						<div class="row">
 							<div class="col-md-5 col-md-offset-1">
-								<h2 class="title text-left">Risk-free conditional smart payments</h2>
-								<p class="lead text-left">Byteball allows you to do something that traditional currencies can't: <a href="https://medium.com/byteball/making-p2p-great-again-fe9e20546a4a"
-								 target="_blank">conditional payments.</a></p>
+								<h2 class="title text-left">Bezrizikové podmíněné chytré platby</h2>
+								<p class="lead text-left">Byteball vám umožní něco, co tradiční měny nedovedou: <a href="https://medium.com/byteball/making-p2p-great-again-fe9e20546a4a"
+								 target="_blank">podmíněné platby.</a></p>
 
-								<p>You set a condition when the payee receives the money. If the condition is not met, you get your money back.</p>
+								<p>Nastavíte podmínku, za které má příjemce platbu obdržet. Pokud podmínka není splněna, dostanete své peníze zpátky.</p>
 								<div class="space"></div>
 							</div>
 							<div class="col-md-6">
@@ -199,8 +199,8 @@
 						 <div class="space"></div> 
 						<div class="row">
 							<div class="col-md-4 col-md-push-6">
-							<h2 class="title text-left">P2P insurance</h2>
-							<p class="lead text-left">Hedge against negative events.</p>
+							<h2 class="title text-left">P2P pojištění</h2>
+							<p class="lead text-left">Zajistěte se proti nepříjemným událostem.</p>
 							<p>Buy insurance from peers to get paid in case a negative event occurs. Sell insurance for profit.</p>
 							<p>Insurance is just <a href="https://medium.com/byteball/making-p2p-great-again-episode-iv-p2p-insurance-cbbd1e59d527"
 								 target="_blank">a simple smart contract</a> that can be unlocked by the insured &mdash; if the event in question
