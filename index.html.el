123d5cf1cbea4a5a7965f8228b589a7a5b857cc1
translationsproxy <to.ny991@gmail.com>
2018-06-24 19:20:37 +0300

    New translations index.html (Greek)

--- index.html.el
+++ index.html.el
@@ -225,7 +225,7 @@
 				</div>
 
 				<div class="col-md-5 margin-left-7p">
-					<h3 class="section-heading m-b-30 block2-title-margin-top open-sans-bold">ICO Platform</h3>
+					<h3 class="section-heading m-b-30 block2-title-margin-top open-sans-bold">ICO Πλατφόρμα</h3>
 					<ul class="v-list list-text-icon list-text-icon-accordion m-b-20" id="accordion" role="tablist" aria-multiselectable="true">
 						<li class="iconbox iconbox-style-2 iconbox-style-2-with-text">
 							<div class="iconbox-item-title collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse11" aria-expanded="false" aria-controls="collapse11">
