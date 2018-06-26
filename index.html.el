31d1b3069ce98f1f3664a7414a909ad7a430d115
translationsproxy <to.ny991@gmail.com>
2018-06-26 17:02:49 +0300

    New translations index.html (Greek)

--- index.html.el
+++ index.html.el
@@ -225,12 +225,12 @@
 				</div>
 
 				<div class="col-md-5 margin-left-7p">
-					<h3 class="section-heading m-b-30 block2-title-margin-top open-sans-bold">ICO Πλατφόρμα</h3>
+					<h3 class="section-heading m-b-30 block2-title-margin-top open-sans-bold">Πλατφόρμα ICO</h3>
 					<ul class="v-list list-text-icon list-text-icon-accordion m-b-20" id="accordion" role="tablist" aria-multiselectable="true">
 						<li class="iconbox iconbox-style-2 iconbox-style-2-with-text">
 							<div class="iconbox-item-title collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse11" aria-expanded="false" aria-controls="collapse11">
 								<i class="iconbox-icon"></i>
-								<span class="iconbox-text iconbox-text-100 open-sans-regular">Η μοναδική ICO πλατφόρμα της αγοράς που προσφέρει τη δυνατότητα αυτοματοποιημένων συμμορφούμενων πωλήσεων tokens ασφαλείας.</span>
+								<span class="iconbox-text iconbox-text-100 open-sans-regular">Η μοναδική πλατφόρμα ICO της αγοράς που προσφέρει τη δυνατότητα αυτοματοποιημένων συμμορφούμενων πωλήσεων tokens ασφαλείας.</span>
 							</div>
 						</li>
 						<li class="iconbox iconbox-style-2 iconbox-style-2-with-text">
